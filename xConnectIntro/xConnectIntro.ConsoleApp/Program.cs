b6b3b91a04d31338247b4b2afa8e3a58d7c4593b
Jason Wilkerson <jason.wilkerson@rbaconsulting.com>
2017-10-13 18:35:51 -0500

    searching for contacts

--- xConnectIntro/xConnectIntro.ConsoleApp/Program.cs
+++ xConnectIntro/xConnectIntro.ConsoleApp/Program.cs
@@ -14,22 +14,47 @@ namespace xConnectIntro.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var config = new XConnectClientConfiguration(
-                new XdbRuntimeModel(CollectionModel.Model),
-                new Uri("https://sc9_xconnect"),
-                new Uri("https://sc9_xconnect"));
+            //AddContact();
+            //SearchContacts();
+            GetContact();
 
-            try
-            {
-                config.Initialize();
-            }
-            catch (XdbModelConflictException ex)
-            {
-                Console.WriteLine(ex.Message);
-                return;
-            }
+            Console.ReadLine();
+        }
 
-            using (var client = new XConnectClient(config))
+        private static async void SearchContacts()
+        {
+            using (var client = GetClient())
+            {
+                var queryable = client.Contacts
+                    .Where(c => c.Interactions.Any(x => x.StartDateTime > DateTime.UtcNow.AddDays(-30)))
+                    .WithExpandOptions(new ContactExpandOptions( "Personal" ));
+
+                var results = await queryable.ToSearchResults();
+                var contacts = await results.Results.Select(x => x.Item).ToList();
+                foreach (var contact in contacts)
+                {
+                    Console.WriteLine($"{contact.Personal().FirstName} {contact.Personal().LastName}");
+                }
+            }
+        }
+
+        private static void GetContact()
+        {
+            using (var client = GetClient())
+            {
+                var contactReference = new IdentifiedContactReference("twitter", "longhorntaco");
+                var contact = client.Get(contactReference, new ExpandOptions() { FacetKeys = { "Personal" } });
+
+                if (contact != null)
+                {
+                    Console.WriteLine($"{contact.Personal().FirstName} {contact.Personal().LastName}");
+                }
+            }
+        }
+
+        private static void AddContact()
+        {
+            using (var client = GetClient())
             {
                 var identifiers = new ContactIdentifier[]
                 {
@@ -51,6 +76,26 @@ namespace xConnectIntro.ConsoleApp
                 client.AddContact(contact);
                 client.Submit();
             }
+        }
+
+        private static XConnectClient GetClient()
+        {
+            var config = new XConnectClientConfiguration(
+                new XdbRuntimeModel(CollectionModel.Model),
+                new Uri("https://sc9_xconnect"),
+                new Uri("https://sc9_xconnect"));
+
+            try
+            {
+                config.Initialize();
+            }
+            catch (XdbModelConflictException ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw;
+            }
+
+            return new XConnectClient(config);
         }
     }
 }
