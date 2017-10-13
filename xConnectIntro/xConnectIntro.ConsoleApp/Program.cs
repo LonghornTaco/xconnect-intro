ef91a536d292c2c96b72d9f877abf33b55c6f40b
Jason Wilkerson <jason.wilkerson@rbaconsulting.com>
2017-10-12 23:17:48 -0500

    creating a new contact

---
+++ xConnectIntro/xConnectIntro.ConsoleApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.XConnect;
+using Sitecore.XConnect.Client;
+using Sitecore.XConnect.Collection.Model;
+using Sitecore.XConnect.Schema;
+
+namespace xConnectIntro.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new XConnectClientConfiguration(
+                new XdbRuntimeModel(CollectionModel.Model),
+                new Uri("https://sc9_xconnect"),
+                new Uri("https://sc9_xconnect"));
+
+            try
+            {
+                config.Initialize();
+            }
+            catch (XdbModelConflictException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+
+            using (var client = new XConnectClient(config))
+            {
+                var identifiers = new ContactIdentifier[]
+                {
+                    new ContactIdentifier("twitter", "longhorntaco", ContactIdentifierType.Known),
+                    new ContactIdentifier("domain", "longhorn.taco", ContactIdentifierType.Known)
+                };
+                var contact = new Contact(identifiers);
+
+                var personalInfoFacet = new PersonalInformation
+                {
+                    FirstName = "Longhorn",
+                    LastName = "Taco"
+                };
+                client.SetFacet<PersonalInformation>(contact, PersonalInformation.DefaultFacetKey, personalInfoFacet);
+
+                var emailFacet = new EmailAddressList(new EmailAddress("longhorn@taco.com", true), "twitter");
+                client.SetFacet<EmailAddressList>(contact, EmailAddressList.DefaultFacetKey, emailFacet);
+
+                client.AddContact(contact);
+                client.Submit();
+            }
+        }
+    }
+}
