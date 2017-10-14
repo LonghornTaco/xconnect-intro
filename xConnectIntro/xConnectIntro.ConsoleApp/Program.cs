5c07b8b879ace2c20ef32e8108e89fb76e449ff6
Jason Wilkerson <jason.wilkerson@rbaconsulting.com>
2017-10-14 15:34:14 -0500

    adding an interaction to a Contact'

--- xConnectIntro/xConnectIntro.ConsoleApp/Program.cs
+++ xConnectIntro/xConnectIntro.ConsoleApp/Program.cs
@@ -16,9 +16,37 @@ namespace xConnectIntro.ConsoleApp
         {
             //AddContact();
             //SearchContacts();
-            GetContact();
+            //GetContact();
+            AddInteraction();
 
             Console.ReadLine();
+        }
+
+        private static void AddInteraction()
+        {
+            using (var client = GetClient())
+            {
+                var contactReference = new IdentifiedContactReference("twitter", "longhorntaco");
+                var contact = client.Get(contactReference, new ExpandOptions() { FacetKeys = { "Personal" } });
+
+                if (contact != null)
+                {
+                    // Item ID of the "Enter Store" Offline Channel at 
+                    // /sitecore/system/Marketing Control Panel/Taxonomies/Channel/Offline/Store/Enter store
+                    var enterStoreChannelId = Guid.Parse("{3FC61BB8-0D9F-48C7-9BBD-D739DCBBE032}"); 
+                    var userAgent = "xConnectIntro Console App";
+
+                    var interaction = new Interaction(contact, InteractionInitiator.Contact, enterStoreChannelId, userAgent);
+
+                    var productPurchaseOutcomeId = Guid.Parse("{9016E456-95CB-42E9-AD58-997D6D77AE83}");
+                    var outcome = new Outcome(productPurchaseOutcomeId, DateTime.UtcNow, "USD", 42.95m);
+
+                    interaction.Events.Add(outcome);
+
+                    client.AddInteraction(interaction);
+                    client.Submit();
+                }
+            }
         }
 
         private static async void SearchContacts()
